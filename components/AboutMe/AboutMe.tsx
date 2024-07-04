514036953ae3d00dac92bce42056a9ca77da31a3
Douaekanjaa <kanjaadouae1@gmail.com>
2024-07-04 21:03:44 +0100

    up

--- components/AboutMe/AboutMe.tsx
+++ components/AboutMe/AboutMe.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Image from "next/image";
-import { Socials } from "../../constants/index";
+import { Socials } from "@/constants/index";
 
 const AboutMe = () => {
   return (
